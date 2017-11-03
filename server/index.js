255e7843f0cda8ed9a44d3aef15b402b12a47447
Matt Claypotch <thepotch@gmail.com>
2017-11-03 15:13:14 -0700

    serve data blobs at the appropriate urls

--- server/index.js
+++ server/index.js
@@ -5,28 +5,28 @@ const path = require('path');
 
 const DATA_FORMAT = 'json';
 const API_KEY = process.env.API_KEY;
-//const url = `https://sql.telemetry.mozilla.org/api/queries/3648/results.${DATA_FORMAT}?api_key=${API_KEY}`;
-const dataSources = require('./src/dataURIs.json')
 
-const urls = Object.keys(dataSources)
-let data;
+const dataSources = require('../src/dataURIs.json');
+const urls = Object.keys(dataSources);
+
+let data = {};
 
 function updateData() {
   console.log('updating...');
 
-  urls.forEach(url=>{
-    request(dataSources[url], function(error, response, body) {
-    if (error) {
-      console.log(error);
-      return;
-    }
-    try {
-      data = JSON.parse(body);
-    } catch (e) {
-      console.error(e);
-    }
+  urls.forEach(url => {
+    request(dataSources[url].replace('${API_KEY}', API_KEY), function(error, response, body) {
+      if (error) {
+        console.error(`failed to load response for ${url}:`, error);
+        return;
+      }
+      try {
+        data[url] = JSON.parse(body);
+      } catch (e) {
+        console.error(`failed to parse response for ${url}:`, e);
+      }
+    });
   });
-  })
 }
 
 setInterval(updateData, 5 * 60 * 1000);
@@ -38,8 +38,14 @@ app.get('/', function(request, response) {
   response.sendFile(path.join(__dirname, '..', 'index.html'));
 });
 
-app.get('/data', function(request, response) {
-  response.json(data);
+app.get('/data/:series.json', function(request, response) {
+  let series = request.params.series;
+  if (series in data) {
+    response.json(data[series]);
+  } else {
+    response.status(404);
+    response.end();
+  }
 });
 
 var listener = app.listen(process.env.PORT, function() {
--- src/index.jsx
+++ src/index.jsx
@@ -81,7 +81,7 @@ function mainDisclaimer() {
 
 render(
     <GraphicDisplay>
-        <Header title='Firefox Quantum' subtitle='release metrics'  img='ff-quantum.png' />
+        <Header title='Firefox Quantum' subtitle='release metrics'  img='static/ff-quantum.png' />
         <ToplineRow>
             <ToplineElement value={(()=>{
                 var msPerDay = 8.64e7;
