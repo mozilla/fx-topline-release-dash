f1f635c2d3834a9cbed9b1a53022ebfe5bf35391
hamilton <hamilton.ulmer@gmail.com>
2017-11-03 11:43:11 -0700

    added dataSources.js so server component can use those as well

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import {render} from 'react-dom'
+
+import {dataSources as displays} from './dataSources.js'
+
 import {GraphicDisplayStyle,
     GraphicDisplay,
     DisplayRow,
@@ -17,7 +20,7 @@ import {GraphicDisplayStyle,
 
 const WHICH_VERSION = 'release'
 const RELEASE_DATE = new Date('2017-11-14')
-const DATA_FORMAT = 'json'
+
 
 function qv(variable) {
     var query = window.location.search.substring(1)
@@ -32,154 +35,7 @@ function qv(variable) {
     return out
 }
 
-function handleFormat(data) {
-    var out = DATA_FORMAT === 'json' ? data.query_result.data.rows : data
-    return out
-}
 
-var displays = {
-    successfulInstalls: {
-        id: "successfulInstalls",
-        title: "Successful Installs",
-        description: "the percentage of attempted installs that are successful",
-        plotArgs: {format: 'Percentage'},
-        source: "https://sql.telemetry.mozilla.org/queries/3648#7201",
-        apiURI: `https://sql.telemetry.mozilla.org/api/queries/3648/results.${DATA_FORMAT}?api_key=NNEptnmnH7Wt7XbXkzMwVEEdKOCkwUZkOIuA1hcs`,
-        format: DATA_FORMAT,
-        preprocessor: data => {
-            data = handleFormat(data)
-
-            var tmp = data.reduce((obj, d) => {
-                if (!obj.hasOwnProperty(d.day)) obj[d.day] = {}
-                obj[d.day][d.succeeded] = d.instances
-                return obj
-            }, {})
-
-            var out = Object.keys(tmp).reduce((arr,d)=> {
-                var di = tmp[d]
-                var True = di[true] || 0
-                var False = di[false] || 0
-                arr.push({day: d, successfulInstalls: True  / (True + False)})
-                return arr
-            }, [])
-
-            out = MG.convert.date(out, 'day')
-
-            return out
-        },
-        xAccessor: 'day',
-        yAccessor: 'successfulInstalls'
-    },
-
-    uptake: {
-        title: 'Uptake',
-        id: "uptake",
-        plotArgs: {format: 'Percentage'},
-        description: 'the percentage of our Daily Active Users (DAUs) coming from Firefox 57 profiles',
-        polling: ()=>{},
-        source: "https://sql.telemetry.mozilla.org/queries/48512/source#130992",
-        apiURI: `https://sql.telemetry.mozilla.org/api/queries/48552/results.${DATA_FORMAT}?api_key=OjrW1fBixUDsqinC00X3P6JPGTk7A9iNjwYeRd8h`,
-        format: DATA_FORMAT,
-        preprocessor: data => {
-            data = handleFormat(data)
-            data = MG.convert.date(data, 'd', '%Y%m%d')
-            //data = MG.convert.number(data, 'uptake')
-            data = data.map(d => {
-                d.uptake = d.uptake / 100
-                return d
-            })
-            return data
-        },
-        xAccessor: 'd',
-        yAccessor: 'uptake'
-    },
-
-    newUsers: {
-        title: "New Users",
-        id: "newUsers",
-        description: "The number of new Firefox 57 profiles",
-        format: DATA_FORMAT,
-        preprocessor: (data) => {
-            data = handleFormat(data)
-            data = MG.convert.date(data, 'submission', '%Y%m%d')
-            return data
-        },
-        xAccessor: 'submission',
-        yAccessor: 'new_profiles',
-        apiURI: `https://sql.telemetry.mozilla.org/api/queries/48504/results.${DATA_FORMAT}?api_key=xPo352uOKROX3xktCOU8t38wgTSDkOdWZWLRamSt`,
-        source: "https://sql.telemetry.mozilla.org/queries/48504/source#130999"
-    },
-
-    dau: {
-        title: "Daily Active Users (DAUs)",
-        id: 'dau',
-        description: "Daily Active Users (DAU), smoothed over the previous 7 days",
-        apiURI: `https://sql.telemetry.mozilla.org/api/queries/48553/results.${DATA_FORMAT}?api_key=EBSmbDQLOUxuqqTXIjax1ARNUYRcqn9y7UiHca3r`,
-        source: "https://sql.telemetry.mozilla.org/queries/48553/source",
-        format: DATA_FORMAT,
-        preprocessor: (data) => {
-            data = handleFormat(data)
-            data = MG.convert.date(data, 'date', '%Y%m%d')
-            return data
-        },
-        xAccessor: 'date',
-        yAccessor: 'smooth_dau'
-    },
-
-    stability: {
-        title: "Crash Rate",
-        description: "(Browser Crashes + Content Crashes - Content Shutdown Crashes) per 1,000 hours",
-        format: DATA_FORMAT,
-        xAccessor: 'activity_date',
-        yAccessor: 'crash_rate',
-        preprocessor: (data) => {
-            data = handleFormat(data)
-            data = MG.convert.date(data, 'activity_date')
-            data = data.map(d=>{
-                d.crash_rate = d['M + C - S'] / d.usage_khours
-                d.date = d.activity_date
-                return d
-            })
-            data = data.filter(d=>d.channel === WHICH_VERSION && d.build_version=='57.0' && d.date > new Date('2017-10-01'))
-            return data
-        },
-        apiURI: `https://sql.telemetry.mozilla.org/api/queries/1092/results.${DATA_FORMAT}?api_key=f7dac61893e040ca59c76fd616f082479e2a1c85`
-    },
-
-    pagesVisited: {
-        title: "Total Pages Visited",
-        id: "pagesVisited",
-        description: "Total number of URIs visited",
-        format: DATA_FORMAT,
-        apiURI: `https://sql.telemetry.mozilla.org/api/queries/48587/results.${DATA_FORMAT}?api_key=OuKxbk1VK8xWdb54MtLMfTfAbRa4sOlQU4mt38Cv`,
-        source: 'https://sql.telemetry.mozilla.org/queries/48587/source',
-        preprocessor: (data) => {
-            data = handleFormat(data)
-            data = MG.convert.date(data, 'date', '%Y%m%d')
-            return data
-        },
-        xAccessor: 'date',
-        yAccessor: ['avg_uri_new', 'avg_uri_all'],
-        plotArgs: {legend: ['Quantum', 'All']}
-    },
-
-    sessionHours: {
-        title: "Total Session Hours",
-        id: "sessionHours",
-        description: "Total number of hours logged by Firefox 57 profiles",
-        apiURI: `https://sql.telemetry.mozilla.org/api/queries/48583/results.${DATA_FORMAT}?api_key=UTF3SaBOqusUa5fP4BstQ5M9YhlLIejS0RSCyIQB`,
-        source: 'https://sql.telemetry.mozilla.org/queries/48583/source',
-        format: DATA_FORMAT,
-        preprocessor: data => {
-            data = handleFormat(data)
-            data = MG.convert.date(data, 'date', '%Y%m%d')
-            return data
-        },
-        xAccessor: 'date',
-        yAccessor: ['avg_subsess_hours_new', 'avg_subsess_hours_all'],
-        plotArgs: {'legend': ['Quantum', 'All']}
-    }
-}
 
 function dataGraphicCell(args) {
     var disclaimer = args.hasOwnProperty('disclaimer') ? <GraphicDisclaimer> <span style={{fontWeight:900, paddingRight:10}}>NOTE</span>  {args.disclaimer} </ GraphicDisclaimer> : ''
